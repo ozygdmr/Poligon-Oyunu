2878d7a3393981813d0663c7d7b83767d1133e9d
ozygdmr <uozdemir1981@gmail.com>
2025-05-21 22:43:00 +0300

    First

---
+++ Poligon Oyunu/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Poligon_Oyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(164)))), ((int)(((byte)(168)))));
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(-2, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(805, 31);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(762, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(32, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Impact", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(616, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "12";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Franklin Gothic Medium", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(651, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "60";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel2.Location = new System.Drawing.Point(13, 67);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(558, 371);
+            this.panel2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(616, 176);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 18);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Son Rekor:0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(179, 43);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(175, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Ekranda Çıkan Mavi İşaretlere Tıkla";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Poligon Oyunu/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Poligon_Oyunu
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int isd = 0;
+        int mousePosX;
+        int mouseposY;
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isd = 1;
+            mousePosX = Control.MousePosition.X;
+            mouseposY = Control.MousePosition.Y;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(isd == 1)
+            {
+                this.Location = new Point(this.Location.X+(Control.MousePosition.X-mousePosX),this.Location.Y+(Control.MousePosition.Y-mouseposY));
+                mousePosX = Control.MousePosition.X;
+                mouseposY = Control.MousePosition.Y;
+            }
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isd = 0;
+        }
+        int saniye = 0;
+        int sonrekor = 0;
+        int milisaniye = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            saniye = Int32.Parse(mywatch.ElapsedMilliseconds.ToString()) / 1000;
+            milisaniye = Int32.Parse(mywatch.ElapsedMilliseconds.ToString()) % 1000;
+            
+            
+            label3.Text = milisaniye.ToString();
+            label2.Text = saniye.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        Button circle = new Button();
+        Stopwatch mywatch = new Stopwatch();
+        Button startbutt = new Button();
+        string executeloc;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            
+
+            startbutt.Visible = true;
+            startbutt.Location = new Point(220, 150);
+            startbutt.Width = 100;
+            startbutt.Height = 100;
+            startbutt.Text = "Basla";
+            startbutt.Name = "Baslabut1";
+            startbutt.BackColor = Color.Green;
+            startbutt.MouseClick += Startbutt_MouseClick;
+            panel2.Controls.Add(startbutt);
+            panel2.Refresh();
+            executeloc = Assembly.GetExecutingAssembly().Location;
+            executeloc = executeloc.Replace("\\Poligon Oyunu.exe", "\\mycircle.png");
+        }
+        Random myrand = new Random();
+        int sayim = 0;
+        private void Startbutt_MouseClick(object sender, MouseEventArgs e)
+        {
+            sayim = 0;
+            mywatch = Stopwatch.StartNew();
+            mywatch.Start();
+            circle.Top = myrand.Next(300);
+            circle.Left = myrand.Next(500);
+            circle.Width = 50;
+            circle.FlatAppearance.BorderSize = 0;
+            circle.BackColor = Color.Transparent;
+            circle.Height = 50;
+            circle.FlatStyle = FlatStyle.Flat;
+            circle.Image = Image.FromFile(executeloc);
+            panel2.Controls.Add(circle);
+            panel2.Refresh();
+            circle.MouseClick += CircleClick;
+            panel2.Controls.Remove(startbutt);
+        }
+        
+        private void CircleClick(object sender,EventArgs e)
+        {
+            circle.Top = myrand.Next(300);
+            circle.Left = myrand.Next(500);
+            sayim++;
+            if(sayim >= 30)
+            {
+                mywatch.Stop();
+                panel2.Controls.Clear();
+                MessageBox.Show("Oyun Bitti Skorunuz:" + saniye.ToString() + "saniye," + milisaniye.ToString() + "milisaniye");
+                panel2.Controls.Add(startbutt);
+                if(sonrekor < (saniye * 1000 + milisaniye))
+                {
+                    sonrekor = saniye * 1000 + milisaniye;
+                    label1.Text = "Son Rekor:" + saniye.ToString() + "," + milisaniye.ToString();
+                    
+                }
+            }
+        }
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+        
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            label1.Text = executeloc;
+            
+        }
+    }
+}
